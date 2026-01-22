706329c79810e4ac4acc05b38570e7331dc2c581
busybeedev <ebesuvictor@gmail.com>
2026-01-22 16:23:07 +0100

    Added comments for easy readability

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.querySelector('.grid').addEventListener('click', function (e) {
     if (e.target.tagName === "IMG") {
+
+        // check number of images left
         let noOfImgs = this.querySelectorAll('img').length;
         if (noOfImgs > 1) {
+            // remove clicked image
             let removeImg = e.target.parentNode;
             removeImg.parentNode.removeChild(removeImg);
         } else if (noOfImgs === 1) {
+            // display info about the last image
             let finalImgTitle = e.target.alt;
             document.querySelector('#art p').innerHTML = `You have selected your favourite image: <strong>${finalImgTitle}</strong>`;
         }
